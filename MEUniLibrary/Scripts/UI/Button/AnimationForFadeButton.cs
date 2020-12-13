853222e5f59945d71c03f23356bf816048bdca16
menyawa <41637976+menyawa@users.noreply.github.com>
2020-12-13 11:39:48 +0900

    Add AnimationForFadeButton

---
+++ MEUniLibrary/Scripts/UI/Button/AnimationForFadeButton.cs
@@ -0,0 +1,28 @@
+﻿using MEUniLibrary.UI.Animation;
+using UnityEngine;
+
+namespace MEUniLibrary.UI.Button {
+    /// <summary>
+    /// AnimationForFadeを用いてUIをフェードイン・フェードアウトするクラス
+    /// </summary>
+    public class AnimationForFadeButton : SwitchOpeningAndClosingOfUIButtonBase {
+        [SerializeField] private AnimationForFade animationForFade_;
+
+        new private void Start() {
+            base.Start();
+
+            button_.onClick.AddListener(fade);
+        }
+
+        private void fade() {
+            switch (type_) {
+                case TYPE.OPEN:
+                    animationForFade_.fade();
+                    break;
+                case TYPE.CLOSE:
+                    animationForFade_.fade();
+                    break;
+            }
+        }
+    }
+}
