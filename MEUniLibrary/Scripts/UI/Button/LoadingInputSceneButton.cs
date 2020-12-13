d479561d1c7a6351efae072827d910734aae7414
menyawa <41637976+menyawa@users.noreply.github.com>
2020-12-13 16:30:43 +0900

    Update loading scene button

--- MEUniLibrary/Scripts/UI/Button/LoadingInputSceneButton.cs
+++ MEUniLibrary/Scripts/UI/Button/LoadingInputSceneButton.cs
@@ -6,18 +6,15 @@ namespace MEUniLibrary.UI.Button {
     /// <summary>
     /// 入力窓に入力した名前のシーンをロードするクラス
     /// </summary>
-    public class LoadInputSceneButton : ButtonBase {
+    public class LoadingInputSceneButton : LoadingSceneButton {
         [SerializeField] private TMP_InputField inputField_;
 
-        new private void Start() {
-            base.Start();
-            button_.onClick.AddListener(loadScene);
-        }
+        protected override void loadScene() {
+            sceneName_ = inputField_.text;
 
-        private void loadScene() {
             //どうやらBuildSettingに登録されたシーン一覧を取得できないらしいので、名前比較でビルドに指定されたシーンが含まれているかどうかを見てエラーを弾くことは不可能
             //なので素直に入れられたテキストでシーンを愚直に呼びましょう
-            SceneManager.LoadScene(inputField_.text);
+            base.loadScene();
         }
     }
 }
--- MEUniLibrary/Scripts/UI/Button/LoadingSceneButton.cs
+++ MEUniLibrary/Scripts/UI/Button/LoadingSceneButton.cs
@@ -5,16 +5,18 @@ namespace MEUniLibrary.UI.Button {
     /// <summary>
     /// 予めセットした名前のシーンをロードするボタン
     /// </summary>
-    public class LoadSceneButton : ButtonBase {
-        [SerializeField] private string loadSceneName_;
+    public class LoadingSceneButton : ButtonBase {
+        [SerializeField] protected string sceneName_;
+        //ロード前に遅延を挟める
+        [SerializeField] private float delay_;
 
         new protected void Start() {
             base.Start();
             button_.onClick.AddListener(loadScene);
         }
 
-        private void loadScene() {
-            SceneManager.LoadScene(loadSceneName_);
+        protected virtual void loadScene() {
+            SceneManager.LoadScene(sceneName_);
         }
     }
 }
