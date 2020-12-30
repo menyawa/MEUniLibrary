dc6fd4f661f97993bd4bfe0d3e770338e8141376
menyawa <41637976+menyawa@users.noreply.github.com>
2020-12-30 19:17:52 +0900

    Update MenuByPopUp

--- MEUniLibrary/Scripts/UI/Animation/MenuByPopUp.cs
+++ MEUniLibrary/Scripts/UI/Animation/MenuByPopUp.cs
@@ -6,9 +6,13 @@ namespace MEUniLibrary.UI.Animation {
     /// ポップアップで出てくるメニューのクラス
     /// </summary>
     public class MenuByPopUp : DOTweenAnimationSequenceBase {
+        //メニューの画像それぞれのSprite
         [SerializeField] private Sprite[] menuSprites_;
+        //端から端へのループが可能か(最後まで行ったとき最初に戻るのか)
+        [SerializeField] private bool canLoop_;
         //どのスプライトを指しているかのインデックス
         private int index_;
+
 
         private void Start() {
             resetSprite();
@@ -39,10 +43,17 @@ namespace MEUniLibrary.UI.Animation {
             newMenuRectTransform.localPosition = position; //もう一回代入しないと変な位置に生成される
 
             //呼ばれた前後のメニューの画像を取得
-            //既にインデックスが端の場合、もう片方の端にする
             index_ = callingMenuIsNext ? index_ + 1 : index_ - 1;
-            if (index_ >= menuSprites_.Length) index_ = 0;
-            else if (index_ < 0) index_ = menuSprites_.Length - 1;
+            if (canLoop_) {
+                //ループ可能な場合のみインデックスの端から端への移動が可能
+                if (index_ >= menuSprites_.Length) index_ = 0;
+                else if (index_ < 0) index_ = menuSprites_.Length - 1;
+            } else {
+                //そうでないなら、配列外参照を避けるため端で止める
+                Debug.Log("ループが可能でないのに、メニューの端でループしようとして配列外参照をしようとしています");
+                if (index_ >= menuSprites_.Length) index_ = menuSprites_.Length - 1;
+                else if (index_ < 0) index_ = 0;
+            }
             newMenuImage.sprite = menuSprites_[index_];
 
             initSequence();
@@ -59,12 +70,41 @@ namespace MEUniLibrary.UI.Animation {
         }
 
         /// <summary>
+        /// 現在の画像に前があるかどうか
+        /// </summary>
+        /// <returns></returns>
+        public bool hasPrev() {
+            //ループ可能なら無条件でtrue、そうでないなら左端でなければOK
+            return canLoop_ ? true : index_ > 0;
+        }
+
+        /// <summary>
+        /// 現在の画像に次があるかどうか
+        /// </summary>
+        /// <returns></returns>
+        public bool hasNext() {
+            //ループ可能なら無条件でtrue、そうでないなら右端でなければOK
+            return canLoop_ ? true : index_ < menuSprites_.Length - 1;
+        }
+
+        /// <summary>
         /// スプライトを最初のものにリセットする(付随してスプライトのインデックスもリセット)
         /// </summary>
         public void resetSprite() {
             index_ = 0;
             var currentMenuImage = transform.GetComponentInChildren<UnityEngine.UI.Image>();
             currentMenuImage.sprite = menuSprites_[index_];
+        }
+
+        /// <summary>
+        /// ループが可能か
+        /// </summary>
+        /// <returns></returns>
+        public bool canLoop() {
+            //プロパティによる自動生成ではシリアライズができないためInspectorに表示できない
+            //さりとてメンバそのものをpublicにすると外部から変更ができてしまい、好ましくない
+            //そのため分かりやすさも考えゲッターを用意
+            return canLoop_;
         }
     }
 }
---
+++ MEUniLibrary/Scripts/UI/Button/CursorButtonForMenuByPopUp.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using MEUniLibrary.UI.Animation;
+
+namespace MEUniLibrary.UI.Button {
+    /// <summary>
+    /// ポップアップメニューをスライドするためのカーソルボタンのクラス
+    /// </summary>
+    public class CursorButtonForMenuByPopUp : ButtonBase {
+        [SerializeField] private MenuByPopUp menuByPopUp_;
+        //このボタンのAnimationForScale
+        [SerializeField] private AnimationForScale animationForButtonScale_;
+        //反対側のこのボタン
+        [SerializeField] private CursorButtonForMenuByPopUp oppositeCursorButtonForMenuByPopUp_;
+        //前後のメニューどちらを呼ぶのかのタイプ
+        private enum CALLING_MENU_TYPE {
+            NEXT,
+            PREV
+        }
+        [SerializeField] private CALLING_MENU_TYPE callingMenuType_;
+
+        new private void Start() {
+            base.Start();
+            button_.onClick.AddListener(slide);
+            button_.onClick.AddListener(switchScaleByButtonInMenuEdge);
+        }
+
+        /// <summary>
+        /// メニューをスライドする
+        /// </summary>
+        private void slide() {
+            menuByPopUp_.slide(callingMenuType_ == CALLING_MENU_TYPE.NEXT);
+        }
+
+        /// <summary>
+        /// メニューの端にいるかいないかに合わせこのボタンの最大化・最小化をする
+        /// </summary>
+        public void switchScaleByButtonInMenuEdge() {
+            //ループ可能なメニューなら端というものが存在しないので無条件で返す
+            if (menuByPopUp_.canLoop()) return;
+
+            //参照している方のメニューが存在しているならボタンを最大化(出し)、無いなら(端なら)最小化する
+            //またこのメソッドはボタンを押した際に呼ばれるため、もう一方のボタンでは呼ばれない
+            //なので反対側のボタンの方も呼び出してスケーリングする
+            //最小化する途中で連打されても、メニューの方で端で止まるのでそれを考慮する必要はない
+            switch (callingMenuType_) {
+                case CALLING_MENU_TYPE.NEXT:
+                    if (menuByPopUp_.hasNext()) animationForButtonScale_.expand();
+                    else animationForButtonScale_.minimize();
+                    oppositeCursorButtonForMenuByPopUp_.switchScaleByButtonInMenuEdge();
+                    break;
+                case CALLING_MENU_TYPE.PREV:
+                    if (menuByPopUp_.hasPrev()) animationForButtonScale_.expand();
+                    else animationForButtonScale_.minimize();
+                    oppositeCursorButtonForMenuByPopUp_.switchScaleByButtonInMenuEdge();
+                    break;
+            }
+        }
+    }
+}
