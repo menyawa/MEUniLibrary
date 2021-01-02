240ef3aaba9be2c899027ef33d895d5e55151ee9
menyawa <41637976+menyawa@users.noreply.github.com>
2021-01-02 20:00:52 +0900

    Delete SwitchImageByButton in image

--- MEUniLibrary/Scripts/UI/Button/SwitchImageByButton.cs
+++ MEUniLibrary/Scripts/UI/Button/SwitchImageByButton.cs
@@ -5,7 +5,6 @@ namespace MEUniLibrary.UI.Button {
     /// ボタンの画像を切り替えるボタンの親クラス
     /// </summary>
     public  class SwitchImageByButton : ButtonBase {
-        [SerializeField] private UnityEngine.UI.Image image_;
         [SerializeField] private Sprite nomalSprite_;
         [SerializeField] private Sprite pressedSprite_;
 
@@ -18,14 +17,16 @@ namespace MEUniLibrary.UI.Button {
         /// 画像を切り替える
         /// </summary>
         public void switchImage() {
-            image_.sprite = image_.sprite == nomalSprite_ ? pressedSprite_ : nomalSprite_;
+            var image = GetComponent<UnityEngine.UI.Image>();
+            image.sprite = image.sprite == nomalSprite_ ? pressedSprite_ : nomalSprite_;
         }
 
         /// <summary>
         /// 画像をリセットする
         /// </summary>
         public void resetImage() {
-            image_.sprite = nomalSprite_;
+            var image = GetComponent<UnityEngine.UI.Image>();
+            image.sprite = nomalSprite_;
         }
     }
 }
